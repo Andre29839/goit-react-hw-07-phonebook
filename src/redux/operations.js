09e699e0577dd9aebf08eb0ad52a43c1135f2fa6
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-09-11 16:26:41 +0300

    commit

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk, isRejectedWithValue } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getContacts, postContact, removeContact } from 'service/contactsAPI';
 
 export const fetchContacts = createAsyncThunk(
